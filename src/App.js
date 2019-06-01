428afda6a3e0e38451b49c8f7c3e7aaa73195709
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-05-31 20:47:43 -0400

    Adds ability to edit notes.

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import urlFor from './helpers/urlFor';
 // TODO - Ask Michael about naming convention for file names, functions and variables
 
 class App extends Component {
-  constructor () {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       showNote: false,
       notes: [],
@@ -41,25 +41,36 @@ class App extends Component {
 
   toggleNote = () => {
     this.setState({ showNote: !this.state.showNote});
-  }
+  };
 
   // TODO - Ask Michael if promises are specific to Axios or just the .get, .then .catch format
   getNotes = () => {
     axios.get(urlFor('notes'))
     .then((res) => this.setState( {notes: res.data }))
     .catch((err) => console.log(err.data));
-  }
+  };
 
+
+  // TODO Ask Michael - what's with the weird quotes ` why not just use '
   getNote = (id) => {
     axios.get(urlFor(`notes/${id}`))
     .then ((res) => this.setState({ note: res.data, showNote: true }))
     .catch ((err) => console.log(err.response.data));
-  }
+  };
+
+  performSubmissionRequest = (data, id) => {
+    if (id) {
+      return axios.patch(urlFor(`notes/${id}`), data)
+    }else{
+      return axios.post(urlFor('notes'), data );
+    }
+
+  };
 
   //TODO Ask Michael about the res\err naming and if it is an arbitrary convention
-  submitNote = (data) => {
-    axios.post(urlFor('notes'), data)
-    .then((res) => this.setState({ showNote: false }))
+  submitNote = (data, id) => {
+    this.performSubmissionRequest(data, id)
+    .then(() => this.setState({ showNote: false }))
     .catch((err) => console.log(err.response.data));
   }
 
